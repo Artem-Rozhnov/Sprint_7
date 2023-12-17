c3f19c03308e97582ffeece6e7b31e339469dc78
Artem Rozhnov <bloodypilot@yandex.ru>
2023-12-17 23:47:10 +0300

    first

---
+++ src/main/java/org/example/order/Order.java
@@ -0,0 +1,13 @@
+package org.example.order;
+
+public class Order {
+    private Data data;
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+}
